31de7a830ad838b708a4ee765d62e0c150ae69b1
Amirrezamcp <mousavi.amirreza30@gmail.com>
2026-01-11 10:54:06 +0330

    Route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\v1\Admin\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('users', UserController::class);
