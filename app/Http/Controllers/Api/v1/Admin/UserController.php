c0d0c4c11756ad5ec53faa7cf64274c20771c662
Amirrezamcp <mousavi.amirreza30@gmail.com>
2026-01-11 10:53:20 +0330

    UserController [ index - store - show - update - destroy ]

---
+++ app/Http/Controllers/Api/v1/Admin/UserController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = User::all();
+
+        return response()->json([
+            'data' => $user,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => ['required', 'string', 'min:1', 'max:255'],
+            'last_name' => ['required', 'string', 'min:1', 'max:255'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string', 'min:1', 'max:255'],
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $data = $validator->validated();
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+        return response()->json([
+            'data' => $user,
+            'message' => "Create User: $user->first_name. Successfully!"
+        ]);
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return response()->json([
+            'data' => $user,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => ['required', 'string', 'min:1', 'max:255'],
+            'last_name' => ['required', 'string', 'min:1', 'max:255'],
+            'email' => ['required', 'email'],
+            'password' => ['nullable', 'string', 'min:1', 'max:255'],
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $data = $validator->validated();
+
+        if(isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        $user->update($data);
+        return response()->json([
+            'data' => $user,
+            'message' => "Update User: $user->first_name . $user->lastname. Successfully!"
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json([
+            'massage' => "Delete user: $user->email"
+        ]);
+    }
+}
