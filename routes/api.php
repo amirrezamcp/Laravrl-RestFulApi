fe6e461d93c4573541cc85cfec56bedcb2d03b09
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2026-02-23 19:08:32 +0330

    add Article [model-migration-route-controller-seeder]

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\v1\Admin\UserController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('articles', ArticleController::class)->only(['index']);
