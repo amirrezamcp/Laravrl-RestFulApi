79b3d8830aef18a0c19eb37487028922c8ed9c4d
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2026-02-23 21:07:42 +0330

    fix bug

--- app/Http/Controllers/Api/v1/Admin/ArticleController.php
+++ app/Http/Controllers/Api/v1/Admin/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\v1\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\Article\ArticleListApiResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
