2ce5c2bee212733240cc7691c43f4292fb48df3a
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2026-02-23 19:18:16 +0330

    Update Article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    protected $fillable = [
-        'title',
-        'user_id',
-    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
